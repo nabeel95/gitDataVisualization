4274c30bd3623103dd6ad4999000eb7f4a16640c
Abhishek Thakur <thakura204@gmail.com>
2016-10-06 14:30:02 +0530

    abhikur/nabeel: Added categorise function with test cases

--- scripts/categoriser.js
+++ scripts/categoriser.js
@@ -1,20 +1,34 @@
 var categoriser = {};
 
-categoriser.categoriseByLanguage = function(rawData) {
-	var categorizedData = {};
-	rawData.map(function(repo) {
-		for(i = 0;i<repo.languages.length;i++) {
-		if(!categorizedData[repo.languages[i]]) {
-			categorizedData[repo.languages[i]] = {"USERS": 1, "STARS": repo.stars, "FORKS": repo.forks};
-		}
-		else{
-			categorizedData[repo.languages[i]].USERS++;
-			categorizedData[repo.languages[i]].STARS+=repo.stars;
-			categorizedData[repo.languages[i]].FORKS+=repo.forks;
-		}
-	}
-});
-	return categorizedData;
+categoriser.categoriseByLanguage = function (rawData) {
+    var categorizedData = {};
+    rawData.map(function (repo) {
+        for (i = 0; i < repo.languages.length; i++) {
+            if (!categorizedData[repo.languages[i]]) {
+                categorizedData[repo.languages[i]] = {"USERS": 1, "STARS": repo.stars, "FORKS": repo.forks};
+            }
+            else {
+                categorizedData[repo.languages[i]].USERS++;
+                categorizedData[repo.languages[i]].STARS += repo.stars;
+                categorizedData[repo.languages[i]].FORKS += repo.forks;
+            }
+        }
+    });
+    return categorizedData;
 };
 
+categoriser.categoriseByProject = function(rawData) {
+    var categorizedData = {};
+    rawData.map(function (repo) {
+        var projectName = repo.name.split('/')[1];
+            if (!categorizedData[projectName]) {
+                categorizedData[projectName] = {"CONTRIBUTORS": repo.contributors, "ISSUES": repo.issues};
+            }
+            else {
+                categorizedData[projectName].USERS += repo.contributors;
+                categorizedData[projectName].ISSUES += repo.issues;
+            }
+    });
+    return categorizedData;
+}
 module.exports = categoriser;
--- test/categoriserSpec.js
+++ test/categoriserSpec.js
@@ -2,9 +2,9 @@ var assert = require('assert');
 var categoriser = require('../scripts/categoriser');
 
 describe('categoriser', function () {
-    describe('categoriseByLanguage', function () {
-        it('should categorise data according to the languages', function () {
-            var rawData = [{
+    var rawData;
+    beforeEach(function () {
+            rawData = [{
                 name: 'facebook/nuclied',
                 forks: 3234,
                 stars: 234234,
@@ -20,6 +20,11 @@ describe('categoriser', function () {
                     contributors: 23,
                     issues: 433
                 }];
+    });
+
+    describe('categoriseByLanguage', function () {
+        it('should categorise data according to the languages', function () {
+
             var expected = JSON.stringify({
                     "Javascript": {"USERS": 2, "STARS": 234234 * 2, "FORKS": 5576},
                     "Scala": {"USERS": 1, "STARS": 234234, "FORKS": 2342},
@@ -27,5 +32,16 @@ describe('categoriser', function () {
             var actualData = JSON.stringify(categoriser.categoriseByLanguage(rawData));
             assert.equal(expected, actualData);
         });
+
+     describe('categoriseByProject', function () {
+         it('should categorise data according to projects', function () {
+            var expected = JSON.stringify({
+                "nuclied": {"CONTRIBUTORS": 23, "ISSUES": 200},
+                "gogo": {"CONTRIBUTORS": 23, "ISSUES": 433}
+            })
+             var actualData = JSON.stringify(categoriser.categoriseByProject(rawData));
+             assert.equal(expected, actualData);
+         })
+     })
     });
 });
